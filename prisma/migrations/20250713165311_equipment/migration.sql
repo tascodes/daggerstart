1e5f7f97afd2d6c269f6bd05a416f7eb952acc68
Tas Hasting <tas.hasting@hey.com>
2025-07-13 12:53:23 -0400

    Add db migration for equipment

---
+++ prisma/migrations/20250713165311_equipment/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `Character` ADD COLUMN `equippedArmorName` VARCHAR(191) NULL,
+    ADD COLUMN `equippedPrimaryWeapon` VARCHAR(191) NULL,
+    ADD COLUMN `equippedSecondaryWeapon` VARCHAR(191) NULL;
